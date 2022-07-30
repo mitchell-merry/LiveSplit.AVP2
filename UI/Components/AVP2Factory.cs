b738b261205d1b728ef96937a825e800bceab1b7
Mitchell Merry <mitchellmerry145@gmail.com>
2022-07-31 00:48:42 +1000

    Add UpdateURL

--- UI/Components/AVP2Factory.cs
+++ UI/Components/AVP2Factory.cs
@@ -11,17 +11,8 @@ namespace LiveSplit.UI.Components
         public ComponentCategory Category => ComponentCategory.Control;
         public IComponent Create(LiveSplitState state) => new AVP2Component(state);
         public string UpdateName => ComponentName;
-
-        // Fill in this empty string with the URL of the repository where your component is hosted.
-        // This should be the raw content version of the repository. If you're not uploading this
-        // to GitHub or somewhere, you can ignore this.
-        public string UpdateURL => "";
-
-        // Fill in this empty string with the path of the XML file containing update information.
-        // Check other LiveSplit components for examples of this. If you're not uploading this to
-        // GitHub or somewhere, you can ignore this.
+        public string UpdateURL => "https://raw.githubusercontent.com/mitchell-merry/LiveSplit.AVP2/main/";
         public string XMLURL => UpdateURL + "UI/Components/Updates.xml";
-
         public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
     }
 }
